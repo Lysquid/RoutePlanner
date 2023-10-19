2dc2c31a8ac82f745cd045dfaa555a75d88f4431
Lysquid <romainbenoit21560@gmail.com>
2023-10-19 15:46:23 +0200

    file picker

--- src/main/java/fr/blazanome/routeplanner/view/FxController.java
+++ src/main/java/fr/blazanome/routeplanner/view/FxController.java
@@ -2,11 +2,14 @@ package fr.blazanome.routeplanner.view;
 
 import fr.blazanome.routeplanner.controller.Controller;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+
+import java.io.File;
 
 public class FxController {
 
@@ -26,7 +29,9 @@ public class FxController {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Resource File");
         fileChooser.getExtensionFilters().addAll(new ExtensionFilter("XML file", "*.xml"));
-//        return fileChooser.showOpenDialog(mainStage);
+        Button button = (Button) actionEvent.getSource();
+        File file = fileChooser.showOpenDialog(button.getScene().getWindow());
+        controller.loadMap(file);
     }
 
     public void loadHardCodedMap(ActionEvent actionEvent) {
