6bfac2574f2efcfa27dd826da823899a39946a12
jamessudlow <jamessudlow4@gmail.com>
2023-10-19 10:16:01 +0200

    First version of map class and call

---
+++ src/main/java/fr/blazanome/routeplanner/view/Map.java
@@ -0,0 +1,41 @@
+package fr.blazanome.routeplanner.view;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.Group;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Circle;
+public class Map extends Pane{
+    void drawMap(){
+
+        Group root = new Group();
+        for (int i=0;i<40;i++) {
+            //Setting the properties to a line
+            Line line = new Line();
+            line.startXProperty().bind(heightProperty().divide(2));
+            line.setStartY(10*i);
+            line.setEndX(10*i);
+            line.setEndY(10*i);
+            root.getChildren().add(line);                //Creating a Group
+        }
+        Line line = new Line();
+
+        line.setStartX(0);
+        line.setStartY(0);
+        line.setEndX(0);
+        line.setEndY(0);
+        root.getChildren().add(line);
+        Button bt = new Button();
+        Circle c= new Circle(5);
+        bt.setShape(c);
+        bt.setMaxSize(10,10);
+        bt.setMinSize(10,10);
+        bt.setLayoutX(10);
+        bt.setLayoutY(10);
+        bt.layoutXProperty().bind(widthProperty().divide(2));
+        bt.layoutYProperty().bind(heightProperty().divide(2));
+
+        root.getChildren().add(bt);                //Creating a Group
+        this.getChildren().add(root);
+
+    }
+}
--- src/main/java/fr/blazanome/routeplanner/view/Window.java
+++ src/main/java/fr/blazanome/routeplanner/view/Window.java
@@ -1,30 +1,34 @@
 package fr.blazanome.routeplanner.view;
 
 import javafx.application.Application;
-
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 public class Window extends Application {
-
     @Override
     public void start(Stage stage) {
 
         HBox mainPane = new HBox();
         VBox leftPane = new VBox();
-        StackPane mapPane = new StackPane();
 
-        mapPane.setStyle("-fx-background-color: green;");
-        Label lab1 = new Label("Map");
-        mapPane.getChildren().add(lab1);
-        mapPane.setMinSize(700, 700);
+
+
+
+        //Creating a Scene
+
+        //Setting title to the scene
+
+        //Adding the scene to the stage
+        //stage.setScene(scene);
+
+        //Displaying the contents of a scene
+
+
 
         HBox controlsBox = new HBox();
         Button undoButton = new Button("Undo");
@@ -39,15 +43,17 @@ public class Window extends Application {
         for (Node child : leftPane.getChildren()) {
             VBox.setMargin(child, insets);
         }
-
-        mainPane.getChildren().addAll(leftPane, mapPane);
-
         Scene scene = new Scene(mainPane);
         stage.setTitle("Route Planner");
         stage.setScene(scene);
         stage.show();
+        Map m=new Map();
+        m.setStyle("-fx-background-color: green;");
+        m.setMinSize(500, 500);
+        m.setMaxSize(700, 700);
+        mainPane.getChildren().addAll(leftPane, m);
+        m.drawMap();
     }
-
     public static void main(String[] args) {
         Window.launch();
     }
