0744acaf8897ad678eaeb8df8722be50e68da431
Gartox <ewan71230@gmail.com>
2023-10-19 10:13:50 +0200

    Add javadoc to IMap

--- src/main/java/fr/blazanome/routeplanner/model/IMap.java
+++ src/main/java/fr/blazanome/routeplanner/model/IMap.java
@@ -7,19 +7,46 @@ import java.util.Iterator;
  */
 public interface IMap extends Graph {
 
+    /**
+     * @return an iterator over all segment of the map
+     */
     public Iterator<Segment> getSegments();
 
+    /**
+     * @param start Start vertex number
+     * @param end   End vertex number
+     * @return the <code>Segment</code> between the two vertices if they are
+     *         neighbors and null
+     *         otherwise
+     */
     public Segment getSegment(int start, int end);
 
+    /**
+     * @return the total number of segments
+     */
     public default int getSegmentCount() {
         return this.getVerticesCount();
     }
 
+    /**
+     * @return an iterator over all intersections of the map
+     */
     public Iterator<Intersection> getIntersections();
+
+    /**
+     * @return the total number of intersections
+     */
     public int getIntersectionCount();
 
+    /**
+     * @param vertexId Vertex number
+     * @return the intersection with <code>vertexId</code> or null if it does not
+     *         exist
+     */
     public Intersection getIntersection(int vertexId);
 
-
+    /**
+     * @return The intersection corresponding to the warehouse
+     */
     public Intersection getWarehouse();
 }
