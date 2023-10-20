5d80d7759da2b6933760af3b9187cc209ef9028b
jamessudlow <jamessudlow4@gmail.com>
2023-10-20 20:10:55 +0200

    Rough draft of map drawing. Latitude and longitude aren't calculated and the lines will need to be changed.

--- src/main/java/fr/blazanome/routeplanner/view/Map.java
+++ src/main/java/fr/blazanome/routeplanner/view/Map.java
@@ -1,41 +1,83 @@
 package fr.blazanome.routeplanner.view;
-import javafx.scene.control.*;
-import javafx.scene.layout.Pane;
+
+import fr.blazanome.routeplanner.model.IHMTestMap;
+import fr.blazanome.routeplanner.model.Intersection;
+import fr.blazanome.routeplanner.model.Segment;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.shape.Line;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+
 public class Map extends Pane{
+    private Group root;
+    private Button active=null;
+    double minLat=Double.MAX_VALUE;
+    double minLong=Double.MAX_VALUE;
+    double maxLat=0;
+    double maxLong=0;
     void draw(){
-
-        Group root = new Group();
-        for (int i=0;i<40;i++) {
-            //Setting the properties to a line
-            Line line = new Line();
-            line.startXProperty().bind(heightProperty().divide(2));
-            line.setStartY(10*i);
-            line.setEndX(10*i);
-            line.setEndY(10*i);
-            root.getChildren().add(line);                //Creating a Group
+        IHMTestMap data=new IHMTestMap();
+        root = new Group();
+        Iterable<Intersection> iterableIntersection=data.getIntersections();
+        for (Intersection intersection : iterableIntersection) {
+            minLat=Math.min(minLat,intersection.getLatitude());
+            minLong=Math.min(minLong,intersection.getLongitude());
+            maxLat=Math.max(maxLat,intersection.getLatitude());
+            maxLong=Math.max(maxLong,intersection.getLongitude());
         }
-        Line line = new Line();
-
-        line.setStartX(0);
-        line.setStartY(0);
-        line.setEndX(0);
-        line.setEndY(0);
-        root.getChildren().add(line);
-        Button bt = new Button();
-        Circle c= new Circle(5);
-        bt.setShape(c);
-        bt.setMaxSize(10,10);
-        bt.setMinSize(10,10);
-        bt.setLayoutX(10);
-        bt.setLayoutY(10);
-        bt.layoutXProperty().bind(widthProperty().divide(2));
-        bt.layoutYProperty().bind(heightProperty().divide(2));
-
-        root.getChildren().add(bt);                //Creating a Group
+        this.drawPlainSegments(data.getSegments());
+        this.drawPlainIntersections(iterableIntersection);
+        Intersection warehouseLocation=data.getWarehouse();
+        Rectangle warehouseItem= new Rectangle(8,8);
+        warehouseItem.layoutXProperty().bind(widthProperty().multiply(0.05+0.90*(warehouseLocation.getLatitude()-minLat)/(maxLat-minLat)));
+        warehouseItem.layoutYProperty().bind(heightProperty().multiply(0.05+0.90*(warehouseLocation.getLongitude()-minLong)/(maxLong-minLong)));
+        root.getChildren().add(warehouseItem);
         this.getChildren().add(root);
+
+    }
+    void drawPlainSegments(Iterable<Segment> iterableSegment){
+        //GraphicsContext gc = canvas.getGraphicsContext2D();
+        for (Segment segment : iterableSegment) {
+            //We will need to add
+            Line line = new Line();
+
+
+            //The use of lines is temporary
+            line.startXProperty().bind(widthProperty().multiply(0.05+0.90*(segment.getOrigin().getLatitude()-minLat)/(maxLat-minLat)));
+            line.startYProperty().bind(heightProperty().multiply(0.05+0.90*(segment.getOrigin().getLongitude()-minLong)/(maxLong-minLong)));
+            line.endXProperty().bind(widthProperty().multiply(0.05+0.90*(segment.getDestination().getLatitude()-minLat)/(maxLat-minLat)));
+            line.endYProperty().bind(heightProperty().multiply(0.05+0.90*(segment.getDestination().getLongitude()-minLong)/(maxLong-minLong)));
+            //gc.strokeLine(canvas.getHeight()*(0.05+0.90*(segment.getOrigin().getLatitude()-minLat)/(maxLat-minLat)), canvas.getHeight()*(0.05+0.90*(segment.getOrigin().getLongitude()-minLong)/(maxLong-minLong)),canvas.getHeight()*(0.05+0.90*(segment.getDestination().getLatitude()-minLat)/(maxLat-minLat)), canvas.getHeight()*(0.05+0.90*(segment.getDestination().getLongitude()-minLong)/(maxLong-minLong)));
+            root.getChildren().add(line);
+        }
+        //root.getChildren().add(gc.getCanvas());
+    }
+    void drawPlainIntersections(Iterable<Intersection> iterableIntersection){
+        EventHandler<ActionEvent> event = e -> {
+            Button clicked=(Button)e.getSource();
+            clicked.setStyle("-fx-background-color: #ff0000; ");
+            if(active!=null){
+                active.setStyle("-fx-background-color: #ffffff; ");
+            }
+            active=clicked;
+        };
+        int radius=5;
+        Circle c= new Circle(radius);
+        for (Intersection intersection : iterableIntersection) {
+            Button bt = new Button();
+            bt.setShape(c);
+            bt.setMaxSize(2*radius,2*radius);
+            bt.setMinSize(2*radius,2*radius);
+            bt.layoutXProperty().bind(widthProperty().multiply(0.05+0.90*((intersection.getLatitude()-minLat)/(maxLat-minLat))).subtract(radius));
+            bt.layoutYProperty().bind(heightProperty().multiply(0.05+0.90*((intersection.getLongitude()-minLong)/(maxLong-minLong))).subtract(radius));
+            bt.setOnAction(event);
+            root.getChildren().add(bt);
+        }
 
     }
 }
