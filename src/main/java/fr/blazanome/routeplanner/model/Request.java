72a6689f85fbeaa8ec6189be0303c226cefcbf3c
Lysquid <romainbenoit21560@gmail.com>
2023-10-25 09:09:48 +0200

    removed useless Request class

--- src/main/java/fr/blazanome/routeplanner/model/Request.java
+++
@@ -1,9 +0,0 @@
-package fr.blazanome.routeplanner.model;
-
-import java.util.List;
-
-public class Request {
-    private List<Integer> time_window;
-    private Intersection intersection;
-    private int courier;
-}
