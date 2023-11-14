a62d4841be2e98d51c30589dea9df54d05fbe6cb
Alix Peigue <irobot.moi@gmail.com>
2023-11-14 23:05:28 +0100

    Added delivery Javadoc

--- src/main/java/fr/blazanome/routeplanner/model/Delivery.java
+++ src/main/java/fr/blazanome/routeplanner/model/Delivery.java
@@ -1,7 +1,7 @@
 package fr.blazanome.routeplanner.model;
 
 /**
- * Delivery
+ * A delivery corresponds to a deliveryRequest and has an exact time for the delivery to be done bi the courier
  */
 public class Delivery {
 
@@ -17,27 +17,30 @@ public class Delivery {
         return request;
     }
 
+    /**
+     * @return returns time of the delivery (in minutes)
+     *         example : 120 corresponds to 02:00
+     */
     public int getTime() {
         return time;
     }
 
+    /**
+     * @return the deliveryTime at the format "hh:mm"
+     */
     public String getDeliveryTime() {
         int h = this.time / 60;
         int m = this.time % 60;
         return String.format("%02d:%02d", h, m);
     }
+
+    /**
+     * @return the departure time (5 minutes after the delivery time) at the format "hh:mm"
+     */
     public String getDepartureTime() {
         int h = (this.time + 5)/ 60;
         int m = (this.time + 5) % 60;
         return String.format("%02d:%02d", h, m);
-    }
-
-    public double getLongitude() {
-        return this.request.getLongitude();
-    }
-
-    public double getLatitude() {
-        return this.request.getLatitude();
     }
 
 }
